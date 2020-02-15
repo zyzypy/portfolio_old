cd439a9f9b480422e38130a83b991d1252ef1597
YangZheng <969501808@qq.com>
2020-02-15 10:21:25 +0800

    图标本地引入

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,49 +1,92 @@
 <template>
-    <b-navbar>
-        <template slot="brand">
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img
-                        src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-                        alt="Lightweight UI components for Vue.js based on Bulma"
-                >
-            </b-navbar-item>
-        </template>
-        <template slot="start">
-            <b-navbar-item href="#">
-                Home
-            </b-navbar-item>
-            <b-navbar-item href="#">
-                Documentation
-            </b-navbar-item>
-            <b-navbar-dropdown label="Info">
-                <b-navbar-item href="#">
-                    About
-                </b-navbar-item>
-                <b-navbar-item href="#">
-                    Contact
-                </b-navbar-item>
-            </b-navbar-dropdown>
-        </template>
+    <!-- 公共头部 -->
+    <!-- https://bulma.io/documentation/layout/hero/ -->
+    <div id="header">
+        <!-- 两个色值渐变gradient -->
+        <section class="hero is-primary is-medium">
+            <!-- Hero head: will stick at the top -->
+            <div class="hero-head">
+                <b-navbar>
+                    <template slot="brand">
+                        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                            <img
+                                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                                    alt="Lightweight UI components for Vue.js based on Bulma"
+                            >
+                        </b-navbar-item>
+                    </template>
+                    <template slot="start">
+                        <b-navbar-item href="#/about">
+                            <!-- https://buefy.org/documentation/icon -->
+                            <b-icon
+                                    pack="mdi"
+                                    icon="account"
+                                    size="is-small"
+                            >
+                            </b-icon>
+                            <span>关于我</span>
+                        </b-navbar-item>
+                        <b-navbar-item href="#/apps">
+                            <b-icon
+                                    icon="apps"
+                                    size="is-small"
+                            >
+                            </b-icon>
+                            <span>作品&demo</span>
+                        </b-navbar-item>
+                    </template>
 
-        <template slot="end">
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="button is-light">
-                        Log in
-                    </a>
+                    <template slot="end">
+                        <b-navbar-item tag="div">
+                            <div class="buttons">
+                                <a class="button is-primary" href="#/signup">
+                                    <span><strong>注册</strong></span>
+                                </a>
+                                <a class="button is-light" href="#/signin">
+                                    <span>登录</span>
+                                </a>
+                            </div>
+                        </b-navbar-item>
+                    </template>
+                </b-navbar>
+            </div>
+
+            <!-- Hero content: will be in the middle -->
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title">
+                        Title
+                    </h1>
+                    <h2 class="subtitle">
+                        Subtitle
+                    </h2>
                 </div>
-            </b-navbar-item>
-        </template>
-    </b-navbar>
+            </div>
+
+            <!-- Hero footer: will stick at the bottom -->
+            <div class="hero-foot">
+                <nav class="tabs">
+                    <div class="container">
+                        <ul>
+                            <li class="is-active"><a>Overview</a></li>
+                            <li><a>Modifiers</a></li>
+                            <li><a>Grid</a></li>
+                            <li><a>Elements</a></li>
+                            <li><a>Components</a></li>
+                            <li><a>Layout</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
     export default {
         name: "Header"
     }
+
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
+
 import router from './router'
+// buefy（类似elementUI）  基于bulma（类似bootstrap）的vue组件库
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+// buefy不含图标只负责对接，index.html中外部引入图标库cdn，开发中本地路径需要配置webpack静态文件路由，麻烦，直接main.js引用
+// material design icons v4.9.5
+import '@/assets/css/materialdesignicons.min.css'
 import './plugins/axios'
 
 import App from './App.vue'
 
 
-Vue.use(Buefy)
+Vue.use(Buefy, {
+  defaultIconPack: 'mdi',
+})
 
 Vue.config.productionTip = false
 

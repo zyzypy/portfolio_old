f2e4ee453fcc83305ef7f82f9598e148cf830584
YangZheng <969501808@qq.com>
2020-02-25 19:12:41 +0800

    /works 中部子目录描述（完）

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -31,11 +31,11 @@
             <div class="container is-centered -desc">
                 <div class="columns is-vcentered is-5">
                     <div name="illustration" class="column is-one-thirds level-left">
-                        <img src="../assets/images/linux_server.gif" alt="illustration">
-<!--                        <img :src="activeCatalog.illustration" alt="illustration">-->
+<!--                        <img src="../assets/images/linux_server.gif" alt="illustration">-->
+                        <img :src="activeCatalog.illustration" alt="illustration">
                     </div>
                     <div class="column is-two-thirds level-right">
-                        <p name="introduce" class="has-text-primary is-size-4 has-text-centered"
+                        <p name="introduce" class="has-text-primary is-size-5 has-text-centered"
                             v-html="activeCatalog.introduce"
                         >
                             {{ activeCatalog.introduce }}
@@ -66,42 +66,74 @@
                     {
                         key: 'all',
                         name: '所有',
-                        icon: 'console',
-                        // 静态文件路径会被编译，绑定src时不能用本地路径
-                        illustration: () => import('../assets/images/linux_server.gif'),
-                        introduce: `我拥有7年后端经验....<br>addfd`,
+                        icon: 'apps',
+                        // 静态文件路径会被编译，绑定src时不能用本地路径,使用require
+                        // 起名选择困难症😂 一篇图形近义词的文章 https://blog.csdn.net/u010584319/article/details/82704889
+                        // 插图来自dribbble
+                        illustration: require('../assets/images/cubes.gif'),
+                        introduce: `我已经投身软件行业七年<br>
+                                    在产品、设计、交互、前端、后端开发、服务器等领域任职过或有所研究<br>
+                                    多技能交叉将会产生有趣的项目<br>
+                                    欢迎观看个人项目和开源项目Demo<br>
+                                    `
                     },
                     {
                         key: 'backend',
                         name: '后端',
                         icon: 'console',
-                        /* 起名选择困难症😂 一篇图形近义词的文章 https://blog.csdn.net/u010584319/article/details/82704889 */
-                        illustration: '',
-                        introduce: `我拥有7年后端经验....`,
+                        illustration: require('../assets/images/python.gif'),
+                        introduce: `
+                                    从大学时代的C、C++、Java基础<br>
+                                    到毕业时以Java入行，但工作一段时间后失去兴趣<br>
+                                    再到后来重拾编程，对Python系统自学<br>
+                                    在不同公司项目中积累经验<br>
+                                    少量接触nodejs、go、ruby、abap等其它语言生态<br>
+                                    主语言为Python，我崇尚其简洁的设计哲学，精通核心编程、爬虫、Web开发、ERP、智能硬件等方向<br>
+                                    后端深奥仍在学习进步中
+                                    `
                     },{
-                        key: 'backend',
-                        name: '后端',
-                        icon: 'console',
-                        illustration: '',
-                        introduce: `我拥有7年后端经验....`,
+                        key: 'ui',
+                        name: '设计&交互',
+                        icon: 'brush',
+                        illustration: require('../assets/images/ui_design.gif'),
+                        introduce: `可能有人奇怪编程和设计相差的有些远<br>
+                                    说来话长，有兴趣可以阅读我的故事<br>
+                                    设计是个美丽、充满想象力、比编程愉快的职业<br>
+                                    合理的交互让纠结低效的流程变得舒心<br>
+                                    以下作品时间为2013-2014年，质量跟现在成熟的行业有差距
+                                    `
                     },{
-                        key: 'backend',
-                        name: '后端',
+                        key: 'vuejs',
+                        name: '前端',
                         icon: 'console',
-                        illustration: '',
-                        introduce: `我拥有7年后端经验....`,
+                        illustration: require('../assets/images/frontend.gif'),
+                        introduce: `作为后端一直对前端饶有兴趣，想把一个东西展现出来<br>
+                                    从大学时代html/js jquery/css基础搭配后端做项目<br>
+                                    后来为了让项目好看一点，学习了bootstrap<br>
+                                    2015年，郑州一家互联网教育公司中前端组长推广vue2.0、前后端分离，后端组去前端组帮忙赶进度<br>
+                                    我才知道nodejs、vue新前端时代已到来<br>
+                                    2019下半年，公司前端讲师不够，我兼任带一个前端班学生，从前端基础到node到vue项目<br>
+                                    授课过程中前端基础得到锻炼、框架也随之清晰，开始用起来了
+                                    `
                     },{
-                        key: 'backend',
-                        name: '后端',
-                        icon: 'console',
-                        illustration: '',
-                        introduce: `我拥有7年后端经验....`,
+                        key: 'server',
+                        name: '服务器',
+                        icon: 'server',
+                        illustration: require('../assets/images/linux_server.gif'),
+                        introduce: `大学时只听说过linux好像很🐂🍺的样子，但并不了解<br>
+                                    虚拟机装了一个linux，尝鲜桌面后就不知道干嘛了<br>
+                                    自学python基础后开始玩树莓派<br>
+                                    实验楼的linux基础教程很好<br>
+                                    发觉linux的精髓是命令行，只有前期令人生畏，其余只要搜索教程和变更命令就行<br>
+                                    我想像别人一样做一个可以访问的网站<br>
+                                    云服务器、域名、备案、部署，一套做下来就会了
+                                    `
                     },{
-                        key: 'backend',
-                        name: '后端',
-                        icon: 'console',
-                        illustration: '',
-                        introduce: `我拥有7年后端经验....`,
+                        key: 'hobbies',
+                        name: '爱好',
+                        icon: 'piano',
+                        illustration: require('../assets/images/hobbies.gif'),
+                        introduce: `我拥有7年后端经验....`
                     }
                 ]
             }
@@ -137,8 +169,8 @@
     .-catalogs li {
         flex-grow: 0; /*item排完后是否占用主轴方向空间，默认0，内容多宽占多宽。都为1时等分*/
         flex-shrink: 0; /*shrunk缩放，默认1 true，为0禁止屏幕缩小时也跟着变小*/
-        border-bottom: 1px solid rgb(180,180,180);/*装饰下滑线*/
-        color: rgb(110,110,110);
+        border-bottom: 1px solid rgb(200,200,200);/*装饰下滑线*/
+        color: rgb(130,130,130);
         padding: 0rem 1rem 0rem 0.8rem; /*图标视觉重量轻，内容居中稍偏左*/
         transition: all 0.15s ease-out; /*状态过渡动效*/
     }
@@ -155,8 +187,8 @@
     }
     /* hover状态 */
     .-catalogs li:hover {
-        color: rgb(130,130,130);
-        border-bottom: 1px solid rgb(100, 100, 100);
+        color: rgb(110,110,110);
+        border-bottom: 1px solid rgb(150, 150, 150);
     }
     /* 点击激活状态 */
     .-catalogs li.-active {
